3eb5f92edc5141d733d7ec819578160e0451cf6e
jfabu <justinfabusuyi@gmail.com>
2026-05-06 14:04:07 -0500

    Day 6

---
+++ 06_Day_Tuples/tuples.py
@@ -0,0 +1,26 @@
+empty_tuple = ()
+tpl = ("item1","item2",'item3')
+fruits = ("banana", 'orange','mango','lemon')
+len(fruits)
+
+all_fruits = fruits[0:3]
+all_fruits
+lst= list(fruits)
+print(lst)
+tp3 = tpl+ fruits
+print(tp3)
+
+
+
+empty_tuple = ()
+siblings= ('Chris')
+fruits=('apple','banana','blueberry')
+veggies = ('Corn','broccoli','brussel sprouts','cabbage')
+animal = ('Steak','Chicken','pork','fish')
+food_stuff = fruits+ veggies + animal
+print(food_stuff[0:3])
+print(food_stuff[-3:])
+
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Iceland' in nordic_countries)
